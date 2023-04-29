e1eb8a136d8e9bbf1c6eb42f9bd5bdbabfe15bd7
percivalho <35653644+percivalho@users.noreply.github.com>
2023-04-29 23:26:33 +0100

    main functions done

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,19 +22,120 @@
   // TODO: Add code to display the current date in the header of the page.
 //});
 
-var hours = {
-  start: 9,
-  end: 17
-}
+//var container = $('.container-lg');
+//console.log(container);
+var timeEl = $('.time-block');
 
-var container = $('.container-lg');
-console.log(container);
 
-function appendEntries(){
-  for (var i=hours.start; i<=hours.end; i++){
-    
+var hours = [
+  { id: "hour-9", time: "9AM" },
+  { id: "hour-10", time: "10AM" },
+  { id: "hour-11", time: "11AM" }, 
+  { id: "hour-12", time: "12PM" },
+  { id: "hour-13", time: "1PM" },
+  { id: "hour-14", time: "2PM" },
+  { id: "hour-15", time: "3PM" },
+  { id: "hour-16", time: "4PM" },
+  { id: "hour-17", time: "5PM" },
+  { id: "hour-18", time: "6PM" },
+  { id: "hour-19", time: "7PM" },
+  { id: "hour-20", time: "8PM" },
+  { id: "hour-21", time: "9PM" },
+  { id: "hour-22", time: "10PM" },
+  { id: "hour-23", time: "11PM" }
+];
+
+function updateTextArea(){
+  //var hourInt = parseInt(hour.id.split('-')[1]);
+
+  var container = $(".container-lg").children();
+  //console.log(container);
+
+  for (var i=0; i<container.length; i++){
+    var nowHH =  parseInt(dayjs().format("HH"));
+    //var nowHH =  parseInt(dayjs().format("ss"));
+    //console.log(nowHH);  
+    var timeEl = $(".container-lg").children().eq(i);
+    var hourInt = parseInt(timeEl.attr("id").split('-')[1]);
+    //console.log(hourInt);
+    //console.log($(".container-lg").children().eq(i).children().eq(1));
+    if (nowHH > hourInt){
+      $(".container-lg").children().eq(i).children().eq(1).addClass('past');
+      $(".container-lg").children().eq(i).children().eq(1).removeClass('present');
+      $(".container-lg").children().eq(i).children().eq(1).removeClass('future');
+      //textarea.addClass("past");
+      //console.log("past");
+    } else if (nowHH == hourInt){
+      $(".container-lg").children().eq(i).children().eq(1).addClass('present');
+      $(".container-lg").children().eq(i).children().eq(1).removeClass('past');
+      $(".container-lg").children().eq(i).children().eq(1).removeClass('future');
+      //textarea.addClass("present");
+      //console.log("present");
+    } else if (nowHH < hourInt){
+      $(".container-lg").children().eq(i).children().eq(1).addClass('future');
+      $(".container-lg").children().eq(i).children().eq(1).removeClass('past');
+      $(".container-lg").children().eq(i).children().eq(1).removeClass('present');
+      //textarea.addClass("future");
+      //console.log("future");
+    }
   }
 }
+
+
+function appendEntries(){
+
+  hours.forEach(function(hour) {
+
+    var hourInt = parseInt(hour.id.split('-')[1]);
+    //console.log(hourInt);
+    var nowHH =  parseInt(dayjs().format("HH"));
+    //console.log(nowHH);
+
+    var hoursDiv = $("<div>");
+    hoursDiv.attr("id", hour.id);
+    hoursDiv.addClass("row time-block");
+
+    var hours2Div = $("<div>");
+    hours2Div.addClass("col-2 col-md-1 hour text-center py-3")
+    hours2Div.text(hour.time);
+
+    var textarea = $("<textarea>");
+    textarea.addClass("col-8 col-md-10 description");
+    textarea.attr("rows", "3");
+
+    // retrieve localStorage
+    if (localStorage.getItem(hour.id) != null){
+      textarea.val(localStorage.getItem(hour.id));
+    }
+
+
+    var saveButton = $("<button>");
+    saveButton.addClass("btn saveBtn col-2 col-md-1");
+    saveButton.attr("aria-label", "save");
+
+    var saveIcon = $("<i>");
+    saveIcon.addClass("fas fa-save");
+    saveIcon.attr("aria-hidden", "true");
+
+    // Append the elements to the timeBlock
+    hoursDiv.append(hours2Div).append(textarea).append(saveButton.append(saveIcon));
+
+    // Append the timeBlock to the container-lg
+    $(".container-lg").append(hoursDiv);
+  });
+}
+
+
+function saveLocalStorage(event){
+  var id = $(this).parent().attr("id");
+  var textInput = $(this).parent().children().eq(1).val()
+  console.log(id);
+  console.log(textInput);
+  if (textInput != ''){
+    localStorage.setItem(id, textInput);
+  }
+}
+
 
 function updateTime() {
   var b = dayjs().format();
@@ -48,5 +149,9 @@ function updateTime() {
 $(document).ready(function(){
   updateTime();
   setInterval(updateTime, 1000);
-  appendEntries();
+  setInterval(updateTextArea, 10000); //updateTextArea on every 10min
+  appendEntries();  
+  updateTextArea(); // need append Entries then update text area
+  // Delegate event listener to the parent element, <div id="buttons">
+  $(document).on('click', '.saveBtn', saveLocalStorage);
 });
