3e092c91924b29bfa5b0e9e68de51ae5201ce469
percivalho <35653644+percivalho@users.noreply.github.com>
2023-05-02 22:50:50 +0100

    update comments and README.md

--- README.md
+++ README.md
@@ -1,1 +1,113 @@
 # Week5-Schedule-Input
+
+Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+You'll need to use the [Day.js](https://day.js.org/en/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
+
+## User Story
+
+```md
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Technologies Used
+
+This application is built using the following technologies:
+
+- **HTML5**: Markup language for structuring content.
+- **CSS**: Stylesheet language for styling HTML content.
+- **JavaScript**: Programming language for adding interactivity and dynamic content.
+- **Bootstrap**: Front-end framework for developing responsive and mobile-first websites.
+- **jQuery**: JavaScript library for DOM manipulation and event handling.
+
+
+## Usage
+
+open index.html in Google Chrome (recommended), the Schedule Input is shown as below:
+
+![Schedule Input Start Page](assets/images/start.png)
+
+the current time is updated every second. 
+
+![Current Time update](assets/images/time.png)
+
+The Task input textareas are color-coded, with grey for past events, red for present event and green for future events
+
+![Past Present Future](assets/images/textareas.png)
+
+on clicking the save button after input the task, a message is displayed out.
+
+![Saved Message](assets/images/message.png)
+
+On Refresh the page, start page is shown with saved entries
+![Saved Entries](assets/images/start2.png)
+
+
+## Features
+
+it includes the following features:
+
+1.  The current date, day and time are displayed on start page.
+2.  The current time will be updated on every second.
+3.  Task input text areas are color-coded, with grey for past events, red for present event and gree for future events
+4.  The text areas color-coded is updated per minute
+5.  On clicking the save button after inputting a task, it will save to localStorage
+6.  upon saved to localStorage, a message is displayed out to show the task is saved and the message will fade out in 3 second.
+7.  On refreshing the page, the saved entries still can be shown
+
+
+
+## Tests
+
+Testing done on:
+
+1. testing on current day, date, time display.
+2. testing time update on every second
+3. testing input text areas color-coded.
+4. testing on text areas update on every minute (to test by watching carefully on every 59min per hour and see if any updating on next hour)
+5. testing on input a task and saved, entry is saved in localStoreage.
+6. testing on display saved message after entry saved in localStorage.
+7. testing on refreshing the page, saved entries still can be retrieved from localStorage.
+8. testing on save button on blank input in textArea, nothing saved to localStorage and no saved message shown.
+9. testing on updating the existing saved entries, entry is updated to localStorage.
+10. testing on update entry retrieveal on refreshing the page
+
+
+
+
+## Resources
+
+Link to Deployed Website:
+https://percivalho.github.io/Week4-Coding-Quiz-Challenge/
+
+Link to GitHub repo:
+https://github.com/percivalho/Week4-Coding-Quiz-Challenge.git
+
+
+## License 
+
+![License badge](https://img.shields.io/badge/license-MIT-blue.svg)
+
+
+## Credits and Copyright 
+&copy; Copyright 2023 - Present. Percival Ho
--- assets/js/script.js
+++ assets/js/script.js
@@ -22,11 +22,11 @@
   // TODO: Add code to display the current date in the header of the page.
 //});
 
-//var container = $('.container-lg');
-//console.log(container);
+// global variables
 var timeEl = $('.time-block');
+var messageEl = $('#message');
 
-
+// mapping between id and the time
 var hours = [
   { id: "hour-9", time: "9AM" },
   { id: "hour-10", time: "10AM" },
@@ -45,8 +45,12 @@ var hours = [
   { id: "hour-23", time: "11PM" }
 ];
 
+/**
+ * To update the TextArea showing which is past/present/future
+ * @param None
+ * @returns None
+ */
 function updateTextArea(){
-  //var hourInt = parseInt(hour.id.split('-')[1]);
 
   var container = $(".container-lg").children();
   //console.log(container);
@@ -59,29 +63,30 @@ function updateTextArea(){
     var hourInt = parseInt(timeEl.attr("id").split('-')[1]);
     //console.log(hourInt);
     //console.log($(".container-lg").children().eq(i).children().eq(1));
-    if (nowHH > hourInt){
+    if (nowHH > hourInt){   //past 
       $(".container-lg").children().eq(i).children().eq(1).addClass('past');
       $(".container-lg").children().eq(i).children().eq(1).removeClass('present');
       $(".container-lg").children().eq(i).children().eq(1).removeClass('future');
-      //textarea.addClass("past");
       //console.log("past");
-    } else if (nowHH == hourInt){
+    } else if (nowHH == hourInt){ //present
       $(".container-lg").children().eq(i).children().eq(1).addClass('present');
       $(".container-lg").children().eq(i).children().eq(1).removeClass('past');
       $(".container-lg").children().eq(i).children().eq(1).removeClass('future');
-      //textarea.addClass("present");
       //console.log("present");
-    } else if (nowHH < hourInt){
+    } else if (nowHH < hourInt){  //future
       $(".container-lg").children().eq(i).children().eq(1).addClass('future');
       $(".container-lg").children().eq(i).children().eq(1).removeClass('past');
       $(".container-lg").children().eq(i).children().eq(1).removeClass('present');
-      //textarea.addClass("future");
       //console.log("future");
     }
   }
 }
 
-
+/**
+ * To append all hours entries to display
+ * @param None
+ * @returns None
+ */
 function appendEntries(){
 
   hours.forEach(function(hour) {
@@ -100,15 +105,14 @@ function appendEntries(){
     hours2Div.text(hour.time);
 
     var textarea = $("<textarea>");
-    textarea.addClass("col-8 col-md-10 description");
+    textarea.addClass("col-8 col-md-10 description text-dark");
     textarea.attr("rows", "3");
-    textarea.attr("placeholder", "No event has been scheduled yet");
+    textarea.attr("placeholder", "No task has been scheduled yet");
 
     // retrieve localStorage
     if (localStorage.getItem(hour.id) != null){
       textarea.val(localStorage.getItem(hour.id));
     }
-
 
     var saveButton = $("<button>");
     saveButton.addClass("btn saveBtn col-2 col-md-1");
@@ -126,33 +130,68 @@ function appendEntries(){
   });
 }
 
+/**
+ * To fade in the message on save button, and then fade out in 3s
+ * @param None
+ * @returns None
+ */
+function displayMessage() {
+  messageEl.fadeIn();
+  setTimeout(function() {
+    messageEl.fadeOut();
+  }, 3000);
+}
 
+/**
+ * To save the text Input to local Storage
+ * @param event : for event target, using this
+ * @returns None
+ */
 function saveLocalStorage(event){
   var id = $(this).parent().attr("id");
   var textInput = $(this).parent().children().eq(1).val()
-  console.log(id);
-  console.log(textInput);
+  //console.log(id);
+  //console.log(textInput);
+
+  // only save when there is entry to the schedule
   if (textInput != ''){
     localStorage.setItem(id, textInput);
+    // display saved message after saved to local Storage
+    displayMessage();
   }
 }
 
-
+/**
+ * to update the time using DayJS
+ * @param none
+ * @returns None
+ */
 function updateTime() {
-  var b = dayjs().format();
+  //var b = dayjs().format();
   //console.log(b);
-  var a =  dayjs().format("MMMM D, YYYY HH:mm:ss");
+  //var a =  dayjs().format("MMMM D, YYYY HH:mm:ss");
   //console.log(a);
-  $("#currentDay").text(dayjs().format("MMMM D, YYYY HH:mm:ss"));  
-  //$('#currentDay').addClass('text-info');
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY HH:mm:ss a"));  
 }
 
+// on document ready, perform the following functions
 $(document).ready(function(){
+  // update the time to display out
   updateTime();
+
+  // update time on every 1s
   setInterval(updateTime, 1000);
-  setInterval(updateTextArea, 10000); //updateTextArea on every 10min
+
+  // update the textArea coloring every min 
+  setInterval(updateTextArea, 60000); //updateTextArea on every 1min
+
+  // add entry to div
   appendEntries();  
+
+  // update TextArea coloring
   updateTextArea(); // need append Entries then update text area
+  
   // Delegate event listener to the parent element, <div id="buttons">
   $(document).on('click', '.saveBtn', saveLocalStorage);
+
 });
